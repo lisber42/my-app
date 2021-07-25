ff2cc180d05ff8f512f28105c4384aa5ff53b1ed
Lisber Victores <lvpompa@gmail.com>
2021-07-25 13:59:56 -0300

    Pagination

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Nav from './componets/Nav';
 import SearchBox from './componets/SearchBox';
 import ListFilmes from './componets/ListFilmes';
+import Pagination from './componets/Pagination';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
       movies: [],
-      searchTerm: ''
+      searchTerm: '',
+      totalResults: 0,
+      currentPage: 1
     }
     this.apiKey=process.env.REACT_APP_API
   }
@@ -20,7 +23,7 @@ class App extends React.Component {
     .then(data => data.json())
     .then(data => {
       console.log(data);
-      this.setState({ movies: [...data.results]})
+      this.setState({ movies: [...data.results], totalResults: data.total_results})
     })
   }
 
@@ -29,12 +32,24 @@ class App extends React.Component {
     
   }
 
+  nextPage = (pageNumber) => {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}&page=${pageNumber}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ movies: [...data.results], currentPage: pageNumber})
+      })
+    
+  }
+
   render(){
+    const numberPages = Math.floor(this.state.totalResults / 20);
     return (
       <div className="App">
         <Nav/>
         <SearchBox handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
         <ListFilmes movies={this.state.movies}/>
+        { this.state.totalResults > 20 ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage} /> : ''}
       </div>
     );
   }
--- src/componets/Movie.js
+++ src/componets/Movie.js
@@ -7,7 +7,7 @@ const Movie = (props) => {
             <div className="card">
                 <div clasName="card-image waves-effect waves-block waves-light">
                     {
-                        props.image == null ? < img src={ `https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="card image" style={{ width: "100%", height: 360 }} /> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card image" style={{ width: "100%", height:360}} />
+                        props.image == null ? < img src={ `https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="card " style={{ width: "100%", height: 360 }} /> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt="card " style={{ width: "100%", height:360}} />
                     }
                 </div>
                 <div className="card-content">
